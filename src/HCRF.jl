d8bbf6ac3fc212a7d3a4d117cd4d8a39441ab7de
Balint Takacs <takbal@gmail.com>
2022-11-08 09:52:14 +0100

    exporting transition functions

--- src/HCRF.jl
+++ src/HCRF.jl
@@ -2,9 +2,12 @@ module HCRF
 
 include("types.jl")
 
+include("util.jl")
+
 include("objfunc.jl")
 
 include("model.jl")
-export HCRFModel, fit!, predict, predict_marginals
+export HCRFModel, fit!, predict, predict_marginals,
+       step_transitions, unconstrained_transitions
 
 end # module
